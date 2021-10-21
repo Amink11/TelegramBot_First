62d4dab4a0442f827af44f7f4b275e0cf9eb8a42
amink11 <m.aminkeyvan@gmail.com>
2021-10-21 20:02:11 +0330

    adding answer to bot commands

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
-from
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import logging
+
+
+updater = Updater(token='2099981011:AAEFGqZcvGMcO782cxDlIBR7gzZIlikLoxE', use_context=True)
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my first bot!")
+
+def choose(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Please choose one of these!")
+
+start_handler = CommandHandler('start',start)
+dispatcher.add_handler(start_handler)
+
+start_handler1 = CommandHandler('choose',choose)
+dispatcher.add_handler(start_handler1)
+
+updater.start_polling()
+
+
+
